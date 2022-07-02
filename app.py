7fa3dc1c8eac219df418485fea5deff588ba4b3a
YuZhanHuang <bigjoe1992111@gmail.com>
2022-07-02 16:23:51 +0800

    first stage framework (without simple orm)

--- app.py
+++ app.py
@@ -1,11 +1,37 @@
 from api import API
+from middleware import Middleware
 
-app = API()
+app = API(templates_dir="templates")
+
+
+def custom_exception_handler(request, response, exception_cls):
+    response.text = str(exception_cls)
+
+
+# custom middleware
+class SimpleCustomMiddleware(Middleware):
+    def process_request(self, req):
+        print("Processing request", req.url)
+
+    def process_response(self, req, res):
+        print("Processing response", req.url)
+
+
+app.add_middleware(SimpleCustomMiddleware)
+app.add_exception_handler(custom_exception_handler)
+
+
+def exception_throwing_handler(request, response):
+    raise AttributeError('This handler should not be used.')
 
 
 @app.route('/home')
 def home(request, response):
-    response.text = 'Hello from the HOME page'
+    # response.text = 'Hello from the HOME page'
+    response.body = app.template(
+        "home.html",
+        context={'title': 'Awesome Framework', 'name': 'Bumbo'}
+    )
 
 
 @app.route('/about')
@@ -31,3 +57,26 @@ class BooksResource:
 
     def post(self, req, resp):
         resp.text = "Endpoint to create a book"
+
+
+@app.route("/template")
+def template_handler(req, resp):
+    resp.html = app.template("index.html", context={"name": "Bumbo", "title": "Best Framework"})
+
+
+@app.route("/json")
+def json_handler(req, resp):
+    resp.json = {"name": "data", "type": "JSON"}
+
+
+@app.route("/text")
+def text_handler(req, resp):
+    resp.text = "This is a simple text"
+
+
+# django style
+def handler(req, resp):
+    resp.text = 'sample'
+
+
+app.add_route('/sample', handler, allowed_methods=['get'])
